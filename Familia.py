34f8dbe328191cce191caf307059157fa23b5b7d
Nimsay001 <134750956+Nimsay001@users.noreply.github.com>
2023-06-29 20:12:13 -0300

    Add files via upload

---
+++ Familia.py
@@ -0,0 +1,12 @@
+nome =input()
+nome2 = input()
+nome3 = input()
+print(f"Minha familia é composta por: {nome}. {nome2} e {nome3}")
+
+echo "# Meus-testes" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Nimsay001/Meus-testes.git
+git push -u origin main
